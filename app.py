1970427e77f7990debbac4a92a8825c9251f257c
Mohamed Sherif Abdelaziz <mohamedsherifabdelaziz@Mohameds-MacBook-Air.local>
2019-10-24 17:10:56 +0200

    first

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request
+app = Flask(__name__)
+
+@app.route('/')
+def add():
+    # here we want to get the value of user (i.e. ?user=some-value)
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+    res = num1 + num2
+    return str(res)
+
+@app.errorhandler(404) 
+def not_found_404(e): 
+  return "please re-check your input"
+
+@app.errorhandler(500) 
+def not_found_500(e): 
+  return "please re-check your input"
+
+app.run(port=int("3001"))
