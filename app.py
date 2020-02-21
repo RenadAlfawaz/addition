27a6de76f4a27869afe4940b58722709951170c2
Mohamed Sherif Abdel Aziz <m7sherif@hotmail.com>
2020-02-21 18:49:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def add():
     # here we want to get the value of user (i.e. ?user=some-value)
     num1 = int(request.args.get('num1'))
     num2 = int(request.args.get('num2'))
     res = num1 + num2
+    print("successful")
     return str(res)
 
 @app.errorhandler(404) 
 def not_found_404(e): 
-  return "please re-check your input"
+  return "404 please re-check your input"
 
 @app.errorhandler(500) 
 def not_found_500(e): 
-  return "please re-check your input"
+  return "500 please re-check your input"
 
-app.run(port=int("3001"))
+app.run("0.0.0.0","3000")
